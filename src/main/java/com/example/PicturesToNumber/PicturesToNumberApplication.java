c8e312a470c4a004516d121a38e607a0ca29dfb9
Olenka <namakaolenka22@gmail.com>
2021-07-05 17:46:10 +0300

    Initial commit

---
+++ src/main/java/com/example/PicturesToNumber/PicturesToNumberApplication.java
@@ -0,0 +1,13 @@
+package com.example.PicturesToNumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicturesToNumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicturesToNumberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PicturesToNumber/PicturesToNumberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PicturesToNumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicturesToNumberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
