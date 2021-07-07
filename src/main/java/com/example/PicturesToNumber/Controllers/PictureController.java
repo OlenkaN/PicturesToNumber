446ca6a1c2f79e8e343de20b33b6ac716058c668
Olenka <namakaolenka22@gmail.com>
2021-07-07 12:11:37 +0300

    Controller and Swagger

--- src/main/java/com/example/PicturesToNumber/Controllers/PictureController.java
+++ src/main/java/com/example/PicturesToNumber/Controllers/PictureController.java
@@ -1,6 +1,7 @@
 package com.example.PicturesToNumber.Controllers;
 
 
+import io.swagger.annotations.ApiParam;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,10 +14,12 @@ import java.sql.SQLException;
 public class PictureController {
 
    @RequestMapping(value = "/fileUpload", method = RequestMethod.POST)
-    public @ResponseBody String uploadFile(MultipartFile file, HttpServletRequest req)
+    public @ResponseBody String uploadFile(@RequestPart("file") MultipartFile file, HttpServletRequest req)
             throws SQLException {
 
         // using file.
+       if(file.equals(null))
+           return "FILE EMPTY";
 
         return "success";
     }
