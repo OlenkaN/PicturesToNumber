257d5320fbd31165c3ec6c3fb596647033ed9b65
Olenka <namakaolenka22@gmail.com>
2021-07-07 17:09:12 +0300

    Add javadoc

--- src/main/java/com/example/PicturesToNumber/PicturesToNumberApplication.java
+++ src/main/java/com/example/PicturesToNumber/PicturesToNumberApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
- *
+ * This class is used to run our application
  */
 @SpringBootApplication
 @EnableSwagger2
